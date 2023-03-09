f2980ee348900684e7e807259a3cd3af29e98709
Iskren Aleksandrov <iskren.alexandrov@gmail.com>
2023-03-09 20:20:31 +0200

    Init mail sending & Add controller

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -7,6 +7,8 @@ import { GeneratorModule } from './generator/generator.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from '@/config/typeorm.config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
@@ -17,6 +19,27 @@ import { typeOrmAsyncConfig } from '@/config/typeorm.config';
     MailModule,
     TasksModule,
     GeneratorModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: true,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: '"noreply" tuesplace<noreply@tuesplace.com>',
+      },
+      template: {
+        dir: process.cwd() + '/templates/',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [],
   providers: [],
