9b251d485dd2dbaeae51023b98e76268cc10c5d7
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2023-03-09 17:25:09 +0200

    update auth from component style

---
+++ frontend/src/assets/Constants.js
@@ -0,0 +1,6 @@
+const backgroundColor = '#0A2647';
+const primaryColor = '#144272'
+const basicColor = '#3368b0';
+const textColor = '#F2F2F2'
+
+export { backgroundColor, primaryColor, basicColor, textColor };
--- frontend/src/components/AuthForm.component.jsx
+++ frontend/src/components/AuthForm.component.jsx
@@ -3,9 +3,10 @@ import { Button, Form, Input, Typography } from "antd";
 import Lottie from "lottie-react";
 import Center from "./Center";
 import Security from '../assets/security.json';
+import { basicColor, primaryColor, textColor } from '../assets/Constants';
 
 const validationsRules = { required: true, warningOnly: true };
-const inputWidth = 500;
+const inputWidth = 400;
 
 function AuthForm({ title })
 {
@@ -13,10 +14,15 @@ function AuthForm({ title })
     
     return (
         <Center>
-            <Lottie animationData = {Security} style = {{ width: 260 }}/>
+            <Lottie animationData = {Security} style = {{ width: 300 }}/>
             <div>
                 <Typography>
-                    <Typography.Title level = {1}>{title}</Typography.Title>
+                    <Typography.Title 
+                        style = {{ color: textColor }}
+                        level = {1}
+                    >
+                        {title}
+                    </Typography.Title>
                 </Typography>
 
                 <Form
@@ -30,23 +36,27 @@ function AuthForm({ title })
                 >
 
                     <Form.Item
-                        label = 'Email'
+                        label = {<label style={{ color: textColor }}>Username</label>}
                         name = 'email'
-                        rules={[{ ...validationsRules, message: 'Please enter username!' }]}
+                        rules = {[{ ...validationsRules, message: 'Please enter username!' }]}
                     >
-                        <Input style = {{ minWidth: inputWidth }}/>
+                        <Input style = {{ minWidth: inputWidth, color: primaryColor }}/>
                     </Form.Item>
 
                     <Form.Item
-                        label = 'Password'
+                        label = {<label style={{ color: textColor }}>Password</label>}
                         name = 'password'
-                        rules={[{ ...validationsRules, message: 'Please enter password!' }]}
-                        >
+                        rules = {[{ ...validationsRules, message: 'Please enter password!' }]}
+                    >
                         <Input.Password style={{ minWidth: inputWidth }}/>
                     </Form.Item>
 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
+                    <Form.Item style = {{ display: 'flex', justifyContent: 'center' }}>
+                        <Button 
+                            type="primary" 
+                            htmlType="submit" 
+                            style = {{display: 'flex', alignItems: 'center', backgroundColor: basicColor, padding: 20, }}
+                        >
                             {title == 'Sign up' ? 'Sign up' : 'Sign in'}
                         </Button>
                     </Form.Item>
