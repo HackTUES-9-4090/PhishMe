1f99f07cc5cf32a7ba06da0621059962435e4c4a
DanailYordanov <danail.yordanov1@gmail.com>
2023-03-09 13:38:16 +0200

    Add modules structure

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { AttackModule } from './attack/attack.module';
+import { MailModule } from './mail/mail.module';
+import { TasksModule } from './tasks/tasks.module';
+import { GeneratorModule } from './generator/generator.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AttackModule, MailModule, TasksModule, GeneratorModule],
   controllers: [],
   providers: [],
 })
