df6e936afbda520d7d87d13481f5e5f44e23e394
DanailYordanov <danail.yordanov1@gmail.com>
2023-03-10 22:22:12 +0200

    [BE] Fix CORS

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -6,7 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
