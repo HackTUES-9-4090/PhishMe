255a466986ed846ff061f800362bae464919ce58
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2023-03-10 14:20:36 +0200

    add dropdown for user attacks details

--- frontend/src/components/UserAttacks.component.jsx
+++ frontend/src/components/UserAttacks.component.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { textColor } from '../assets/Constants';
 import styles from './styles/UserAttacks.module.css';
+import GlobalStyles from '../assets/GlobalStyles.module.css';
+import UserAttacksDetails from '../pages/DashBoard/components/UserAttacksDetails.component';
 
-function UserAttacks({ employeeName, phishingsCount })
+function UserAttacks({ employeeName = 'Danail Yordanov', phishingsCount = 1 })
 {
+    const [seeMore, setSeeMore] = useState(false);
+
     return (
         <div className = {styles.userAttackContainer}>
-            <p style = {{ color: textColor, fontSize: 25 }}>{employeeName}</p>
+            <div className = {styles.userData}>
+                <p style = {{ color: textColor, fontSize: 25 }}>{employeeName}</p>
 
-            <div
-                className = {styles.phishings} 
-                style = {{ backgroundColor: phishingsCount > 0 ? '#F0706A' : '#88D453' }}
-            >
-                <p style = {{ color: textColor }}>{phishingsCount}</p>
+                <div
+                    className = {styles.phishings} 
+                    style = {{ backgroundColor: phishingsCount > 0 ? '#F0706A' : '#88D453' }}
+                >
+                    <p style = {{ color: textColor }}>{phishingsCount}</p>
+                </div>
+
             </div>
+
+            {
+                seeMore && 
+                <UserAttacksDetails/>
+            }
+
+            <div className = {GlobalStyles.Center}>
+                <div className = {GlobalStyles.centeredRow}>
+                    <p 
+                        className = {GlobalStyles.Center}
+                        style = {{ color: textColor }}
+                        onClick = {() => setSeeMore(!seeMore)}
+                    >
+                        {seeMore ? 'Close details' : 'Open details'}
+                    </p>
+                </div>
+            </div>
+
         </div>
     )
 }
