dc2a64346eacce6c0c10880cc4fbb98644bcdc61
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2023-03-10 22:15:46 +0200

    Update DashBoard.jsx

--- frontend/src/pages/DashBoard/DashBoard.jsx
+++ frontend/src/pages/DashBoard/DashBoard.jsx
@@ -1,69 +1,49 @@
-import Center from "../../components/Center";
-import { textColor } from "../../utils/Constants";
 import NavbarProvider from "../../hoc/NavbarProvider";
 import Text from "../../components/Text.component";
-import { useState } from "react";
-import { VerticalLeftOutlined } from '@ant-design/icons';
+import { useEffect, useState } from "react";
+import request from '../../utils/requests';
 import styles from './styles/DashBoard.module.css';
 import AttackTable from "./components/AttackTable.component";
 
-function render(text, record)
-{
-  return {
-    props: { style: 
-    {  
-      background: 'rgba(20, 66, 114, 0.8)', 
-      color: textColor, 
-      borderColor: '#0A2647',
-    } },
-    children: <div>{text}</div>
-  } 
-}
 export default function DashBoard({ companyName }) {
   
   const [attackIndex, setAttackIndex] = useState(0);
-  console.log(attackIndex);
+  const [attackData, setAttackData] = useState([]);
 
-  const columns = 
-  [
-    { title: 'Name', width: 300, align: 'center', dataIndex: 'name', key: 'name', render } , 
-    { title: 'Email', width: 300, align: 'center', dataIndex: 'email', key: 'email', render } ,
-    { title: 'Clicked fail', width: 200,align: 'center', dataIndex: 'clickedFail', key: 'clickedFail', render},
-    { title: 'Submit fail', width: 200, align: 'center', dataIndex: 'submitFail', key: 'submitFail', render}
-  ];
+  useEffect(() => 
+  {
+      async function fetchAttackData()
+      {
+        const result = await request('get', '/attack');
+        console.log(result);
+      }
 
-  const data = 
-  [
-    { key: '1', name: 'Danail', clickedFail: 'da', submitFail: 'ne' },
-    { key: '2', name: 'Danail', clickedFail: 'da', submitFail: 'ne' },
-    { key: '3', name: 'Danail', clickedFail: 'da', submitFail: 'ne' },
-    { key: '4', name: 'Danail', clickedFail: 'da', submitFail: 'ne' },
-    { key: '5', name: 'Danail', clickedFail: <VerticalLeftOutlined />, submitFail: 'ne' }
-  ];
+      fetchAttackData();
+  }, []);
 
   return (
-    <NavbarProvider>
-      <div className = {styles.container}>
-        <div className = {styles.centered}>
-          {
-            [1, 2, 3, 4, 5].map((element, index) => 
-            {
-              return(
-                <Text 
-                  key = {index} 
-                  text = {'Attack ' + index} 
-                  onClick = {() => setAttackIndex(index)}
-                />
-              )
-            })
-          }
-          
-        </div>
+      <NavbarProvider>
+        <div className = {styles.container}>
+            <div className = {styles.centered} >
+              {
+                [1, 2, 3, 4, 5].map((element, index) => 
+                {
+                  return(
+                    <Text 
+                      key = {index} 
+                      text = {'Attack ' + index} 
+                      onClick = {() => setAttackIndex(index)}
+                    />
+                  )
+                })
+              }
+            
+            </div>
 
-        <div style = {{ width: '80%', marginLeft: 40, marginRight: 40  }}>
-          <AttackTable/>
-        </div>
-      </div>
-    </NavbarProvider>
+            <div style = {{ width: '80%', marginLeft: 40, marginRight: 40  }}>
+                <AttackTable attackName = {attackIndex}/>
+            </div>
+          </div>
+      </NavbarProvider>
   );
 }
