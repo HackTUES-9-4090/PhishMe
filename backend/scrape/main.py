fb0342b46d068e44fa1ae56d8545bc6e265d075b
Kalo <kaloyan.georgieev@gmail.com>
2023-03-11 00:46:28 +0200

    [SCRAPER] Refactor code and fix bugs
    Bugs were connected to creating directories recursively

--- backend/scrape/main.py
+++ backend/scrape/main.py
@@ -5,7 +5,6 @@ from sys import argv
 from bs4 import BeautifulSoup
 import subprocess
 from urllib.parse import urlparse
-from html.parser import HTMLParser
 import re
 from os import mkdir, path
 import codecs
@@ -14,52 +13,38 @@ if(len(argv) < 3):
     print("Usage ", argv[0], " <URL> <FILENAME>")
     exit()
 
-DIR_NAME = 'app'
-
+ROOT_DIR_NAME = 'app'
 URL = argv[1]
 PARSED_URL = urlparse(URL)
 FILENAME = argv[2]
-if(not(path.exists(DIR_NAME))):
-    mkdir(DIR_NAME)
-
-
-dep = open('dependencies.txt', 'w+')
-if(not(path.exists(DIR_NAME + '/' + FILENAME))):
-    mkdir(DIR_NAME + '/' + FILENAME)
-
-
-class IndexingParser(HTMLParser):
-    def handle_starttag(self, tag, args):
-        print(tag)
-        if(tag != 'a'):
-            for arg in args:
-                par = arg[0]
-                val = arg[1]
-                if((par == 'href' or par == 'src') and val):
-                    parsed_val = urlparse(val)
-                    print(val)
-                    if(parsed_val.scheme):
-                        return
-                    url_id = PARSED_URL.scheme + "://" + PARSED_URL.netloc + (PARSED_URL.path if val[0] != '/' else '') + val
-                    print(url_id)
-                    dep.write(FILENAME + '/' + val + "\n") 
-                    subprocess.run(["node", "./main_downloader.js", url_id, "." + val])
-
-
+WORKING_DIR = ROOT_DIR_NAME + '/' + FILENAME 
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-index = IndexingParser()
+driver = webdriver.Chrome(options=chrome_options)
+
+if(not(path.exists(ROOT_DIR_NAME))):
+    mkdir(ROOT_DIR_NAME)
+if(not(path.exists(WORKING_DIR))):
+    mkdir(WORKING_DIR)
+dep = open(WORKING_DIR + '/' + 'dependencies.txt', 'w+')
 
 def document_initialised(driver):
     return driver.execute_script("return document.readyState == 'complete'")
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(URL)
-WebDriverWait(driver, timeout=10).until(document_initialised)
-#index.feed(driver.page_source)
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-def main():
+def fetch_index_and_refactor():
+    driver.get(URL)
+    WebDriverWait(driver, timeout=10).until(document_initialised)
+    replaced_source = re.sub(r'href="/', 'href="', driver.page_source)
+    replaced_source = re.sub(r'src="/', 'src="', replaced_source)
+    replaced_source = re.sub(r'integrity=".*"', '', replaced_source)
+    #Matches all characters in a group until meeting a (? and more characters until a ") or a " 
+    replaced_source = re.sub(r'href="(.*?)(?:\?.*?"|")', r'href="\1"', replaced_source)
+    with codecs.open(ROOT_DIR_NAME + '/' + FILENAME + '/' + FILENAME + '.html', 'w+', 'utf-8') as f:
+        f.write(replaced_source)
+
+def fetch_static():
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     for tag in soup.find_all(['script', 'link', 'img', 'iframe']):
         print(tag.name)
 
@@ -76,18 +61,12 @@ def main():
         url_id = PARSED_URL.scheme + "://" + PARSED_URL.netloc + (PARSED_URL.path if val[0] != '/' else '') + val
         print(url_id)
         val = re.sub(r'\?.*', '', val)
-        dep.write(FILENAME + '/' + val + "\n") 
-        subprocess.run(["node", "./main_downloader.js", url_id, val, DIR_NAME + '/' + FILENAME])
+        dep.write(WORKING_DIR + ('/' if val[0] != '/' else '') + val + '\t' + url_id + "\n") 
+        subprocess.run(["node", "./fetch.js", url_id, val, WORKING_DIR])
+
+def main():
+    fetch_index_and_refactor()
+    fetch_static()
+
 
 main()
-
-replaced_source = re.sub(r'href="/', 'href="', driver.page_source)
-replaced_source = re.sub(r'src="/', 'src="', replaced_source)
-replaced_source = re.sub(r'integrity=".*"', '', replaced_source)
-#Matches all characters in a group until meeting a (? and more characters until a ") or a " 
-replaced_source = re.sub(r'href="(.*?)(?:\?.*?"|")', r'href="\1"', replaced_source)
-#replaced_source = re.sub('link rel="prefetch" as="script" href=', 'script src=', replaced_source)
-#replaced_source = re.sub('href="')
-#print(replaced_source)
-with codecs.open(DIR_NAME + '/' + FILENAME + '/' + FILENAME + '.html', 'w+', 'utf-8') as f:
-    f.write(replaced_source)
