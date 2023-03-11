a6d9e2c00eaab3b8ce76ad19ab5eac6646046fc7
DanailYordanov <danail.yordanov1@gmail.com>
2023-03-11 07:49:16 +0200

    [BE] Change CORS

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true,
   });
 
