660db19408c91d572a7b603757ce4836751a5573
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2023-03-11 02:21:51 +0200

    [FE] remove linter and update dashboard

--- frontend/src/pages/DashBoard/DashBoard.jsx
+++ frontend/src/pages/DashBoard/DashBoard.jsx
@@ -1,44 +1,47 @@
 import React, { useState, useEffect } from "react";
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import NavbarProvider from "../../hoc/NavbarProvider";
 import Text from "../../components/Text.component";
 import styles from "./styles/DashBoard.module.css";
 import AttackTable from "./components/AttackTable.component";
 import useFetch from "../../hoc/useFetch";
+import { textColor } from "../../utils/Constants";
 
 export default function DashBoard() {
 	const [attackIndex, setAttackIndex] = useState(0);
 	const [attackData, setAttackData] = useState({});
 	const { fetchData } = useFetch();
 
+	const passed = <CheckOutlined style = {{ color: textColor, fontSize: 20}} />
+	const failed = <CloseOutlined style = {{ color: textColor, fontSize: 20 }} />;
+
 	useEffect(() => {
-		async function getAttacks() {
-			let result,
-				dataRows = {},
-				index = 0;
+		async function getAttacks() 
+		{
+			let result, dataRows = {}, index = 0;
 
 			result = await fetchData("get", "/attack");
 
 			for (index in result) {
-				const { name, communicationType, fromName, targets } =
-					result[index];
-
-				dataRows[index] = {
-					name,
-					communicationType,
-					fromName,
-					data: [],
+				const { name, communicationType, fromName, createdAt, targets } = result[index];
+		
+				dataRows[index] = 
+				{ 
+					createdAt, name, communicationType, 
+					fromName, data: [], 
 				};
 
 				targets.forEach(
-					({ name, email, isFailedClick, isFailedSubmit }) => {
+					({ name, email, isFailedClick, isFailedSubmit, generatedEmailContent }) => 
+					{
+						console.log(generatedEmailContent, '------');
 						dataRows[index].data = [
 							...dataRows[index].data,
 							{
-								key: index.toString(),
-								name,
-								email,
-								isFailedClick,
-								isFailedSubmit,
+								key: index.toString(), name, email,
+								isFailedClick: !isFailedClick ? passed : failed,
+								isFailedSubmit: !isFailedSubmit ? passed: failed,
+								generatedEmailContent
 							},
 						];
 					}
@@ -50,25 +53,25 @@ export default function DashBoard() {
 
 		getAttacks();
 	}, []);
-	console.log(attackData);
+	
 	return (
 		<NavbarProvider>
 			<div className={styles.container}>
-				<div className={styles.centered}>
+				<div className={styles.centered} style = {{ flex: 0.7 }}>
 					{Object.keys(attackData).map((element, index) => {
 						return (
 							<Text
-								key={index}
-								className={styles.hover}
-								text={attackData[element].name}
-								onClick={() => setAttackIndex(index)}
+								key = {index}
+								className = {styles.hover}
+								text = {attackData[element].name}
+								onClick = {() => setAttackIndex(index)}
 							/>
 						);
 					})}
 				</div>
 
-				<div style={{ width: "80%", marginLeft: 40, marginRight: 40 }}>
-					<AttackTable attackData={attackData[attackIndex]} />
+				<div estyle={{ width: "80%", marginLeft: 40, marginRight: 40 }}>
+					<AttackTable attackData = {attackData[attackIndex]} />
 				</div>
 			</div>
 		</NavbarProvider>
--- frontend/src/pages/DashBoard/components/AttackTable.component.jsx
+++ frontend/src/pages/DashBoard/components/AttackTable.component.jsx
@@ -4,8 +4,11 @@ import { textColor } from "../../../utils/Constants";
 import Text from "../../../components/Text.component";
 import GlobalStyles from "../../../utils/GlobalStyles.module.css";
 
-function AttackTable({ attackData }) {
+function AttackTable({ attackData }) 
+{
 	console.log(attackData);
+	const basedColumn = { width: 250, align: 'center', render };
+
 	function render(text) {
 		return {
 			props: {
@@ -18,58 +21,56 @@ function AttackTable({ attackData }) {
 			children: <div>{text}</div>,
 		};
 	}
-	const columns = [
-		{
-			title: "Name",
-			width: 300,
-			align: "center",
-			dataIndex: "name",
-			key: "name",
-			render,
-		},
-		{
-			title: "Email",
-			width: 300,
-			align: "center",
-			dataIndex: "email",
-			key: "email",
-			render,
-		},
-		{
-			title: "Clicked fail",
-			width: 200,
-			align: "center",
-			dataIndex: "isFailedClick",
-			key: "isFailedClick",
-			render,
-		},
-		{
-			title: "Submit fail",
-			width: 200,
-			align: "center",
-			dataIndex: "isFailedSubmit",
-			key: "isFailedSubmit",
-			render,
-		},
+	const columns = 
+	[
+		{ title: "Name", dataIndex: "name", key: "name", ...basedColumn },
+		{ title: "Email", dataIndex: "email", key: "email", ...basedColumn },
+		{ title: "Clicked fail", dataIndex: "isFailedClick", key: "isFailedClick", ...basedColumn },
+		{ title: "Submit fail", dataIndex: "isFailedSubmit", key: "isFailedSubmit", ...basedColumn },
 	];
+
+	if (!attackData) return null;
 
 	return (
 		<>
-			<Text text={"Attack "} style={{ fontSize: 20 }} />
+			<Text text={attackData.name} style={{ fontSize: 20 }} />
 			<div
 				className={GlobalStyles.centeredRow}
-				style={{ justifyContent: "space-around" }}
+				style={{ justifyContent: "space-between" }}
 			>
-				<Text text={"Attack name"} style={{ fontSize: 20 }} />
-				<Text text={"Attack name"} style={{ fontSize: 20 }} />
-				<Text text={"Attack name"} style={{ fontSize: 20 }} />
+				<Text
+					text={"Communication type: " + attackData.communicationType}
+					style={{ fontSize: 20 }}
+				/>
+				<Text
+					text={"Send from: " + attackData.fromName}
+					style={{ fontSize: 20 }}
+				/>
+				<Text
+					text={
+						"Sending time: " +
+						new Date(attackData.createdAt).toUTCString()
+					}
+					style={{ fontSize: 20 }}
+				/>
 			</div>
+
+			<div style={{ flex: 0.5, flexWrap: "wrap" }}>
+				<Text text={attackData.generatedEmailContent} />
+			</div>
+
 			<Table
-				dataSource=  {attackData.data}
-				columns = {columns}
-				pagination= { false}
+				dataSource={attackData.data}
+				columns={columns}
+				pagination={false}
+				expandable=
+				{{
+					expandedRowRender: (record) => (
+						<p style={{ margin: 0 }}>{record.generatedEmailContent}</p>
+					),
+					rowExpandable: (record) => record.generatedEmailContent,
+				}}
 			/>
-			;
 		</>
 	);
 }
